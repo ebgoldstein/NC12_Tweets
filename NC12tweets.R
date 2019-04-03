f54f70a4c84c04d4cca3dd6b6af28066a5e0305e
Evan B. Goldstein <ebgoldstein@gmail.com>
2019-04-03 09:25:20 -0400

    download NC12 tweets

---
+++ NC12tweets.R
@@ -0,0 +1,40 @@
+#NC12 tweets
+
+#libraries
+library(rtweet)
+library(tidyverse)
+
+# your app name
+appname <- "YOUR_APP_NAME"
+
+## api key
+key <- "YOUR_KEY"
+
+## api secret key
+secret <- "YOUR_SECRET_KEY"
+
+# create token named "twitter_token"
+twitter_token <- create_token(
+  app = appname,
+  consumer_key = key,
+  consumer_secret = secret)
+
+nc12 <- get_timeline("NCDOT_NC12", n = 3000)
+
+## plot the frequency of tweets for each user over time
+nc12 %>%
+  ts_plot("weeks", trim = 1L) +
+  ggplot2::geom_point() +
+  ggplot2::theme_minimal() +
+  ggplot2::theme(
+    legend.title = ggplot2::element_blank(),
+    legend.position = "bottom",
+    plot.title = ggplot2::element_text(face = "bold")) +
+  ggplot2::labs(
+    x = NULL, y = NULL,
+    title = "Frequency of Tweets by NC12",
+    caption = "\nSource: Data collected from Twitter's REST API via rtweet"
+  )
+
+
+#############
